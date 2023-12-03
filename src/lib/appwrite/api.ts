fdac4466ad4e62df41caab5571eac1227517130f
Tamim Karim <ittamim22@gmail.com>
2023-12-03 09:49:34 +0600

    saving user to database

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,7 +1,8 @@
-import { User } from './../../../node_modules/appwrite/src/models';
+// import { User } from './../../../node_modules/appwrite/src/models';
 import { ID } from 'appwrite';
+
 import { INewUser } from "@/types";
-import { account, avatars } from './config';
+import { account, appwriteConfig, avatars, databases } from './config';
 
 export async function createUserAccount(user: INewUser) {
     try {
@@ -42,8 +43,17 @@ export async function saveUserToDB(user: {
     imageUrl: URL;
     username?: string;
 
-
-
 }) {
     
+    try {
+        const newUser = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            user,
+        )
+        return newUser;
+    } catch (error) {
+        console.log(error);
+    }
 }
